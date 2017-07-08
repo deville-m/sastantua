811457f6c139ac32b462310a40260e9686f752bf
Matthew DEVILLE <mdeville@e1r11p17.42.fr>
2017-07-08 23:11:12 +0200

    lenght fixed

--- sastantua.c
+++ sastantua.c
@@ -6,23 +6,25 @@
 /*   By: mdeville <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/08 00:54:03 by mdeville          #+#    #+#             */
-/*   Updated: 2017/07/08 22:36:08 by mdeville         ###   ########.fr       */
+/*   Updated: 2017/07/08 23:10:25 by mdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 void	ft_putchar(char c);
 
 int		calculate_width(int stage)
 {
-	if (stage < 1)
+	if (stage < 0)
 	{
 		return (-1);
 	}
-	if (stage == 1)
+	if (stage == 0)
 	{
 		return (7);
 	}
-	return (2 * (2 + (stage - 2) / 2) +
+	return (2 * (2 + (stage - 1) / 2) +
 			calculate_width(stage - 1) +
 			2 * (stage + 3));
 }
@@ -36,15 +38,23 @@ void	print_stage(int stage)
 
 	height = 3 + stage;
 	width = calculate_width(stage);
+	printf("largueur : %d\n", width);
 	i = 0;
-	while (i++ < height)
+	while (i < height)
 	{
 		j = 0;
-		while (j++ < height - 1 - i)
+		while (j < height - 1 - i)
+		{
 			ft_putchar(' ');
-		while (j++ < width)
+			j++;
+		}
+		while (j < width)
+		{
 			ft_putchar('*');
+			j++;
+		}
 		ft_putchar('\n');
+		i++;
 	}
 }
 
@@ -52,10 +62,10 @@ void	sastantua(int size)
 {
 	int i;
 
-	i = 1;
-	while (i <= size)
+	i = 0;
+	while (i < size)
 	{
-		print_stage(i);
-		i++;
+		printf("etage : %d\n", i);
+		print_stage(i++);
 	}
 }
