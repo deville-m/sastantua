3621c36f132968d26e1880f763a7e28461ec9f46
Matthew DEVILLE <mdeville@e1r10p17.42.fr>
2017-07-09 13:49:05 +0200

    Normified

--- sastantua.c
+++ sastantua.c
@@ -6,7 +6,7 @@
 /*   By: mdeville <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/08 00:54:03 by mdeville          #+#    #+#             */
-/*   Updated: 2017/07/09 02:38:02 by mdeville         ###   ########.fr       */
+/*   Updated: 2017/07/09 13:46:55 by mdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,33 +27,47 @@ int		calculate_width(int stage)
 			2 * (stage + 3));
 }
 
-int		what_to_print(int i, int j, int *height_width, int pad)
+char	print_door(int stage, int *pos, int *height_width, int pad)
 {
-	if (j < height_width[0] - 1 - i + pad)
+	int center;
+
+	center = calculate_width(stage) / 2;
+	if ((height_width[1] == 7 && pos[0] == 2 + pad && pos[1] == 3 + pad))
+		return ('|');
+	return (center);
+}
+
+char	what_to_print(int stage, int *pos, int *height_width, int pad)
+{
+	char c_door;
+
+	c_door = print_door(stage, pos, height_width, pad);
+	if (c_door == '$' || c_door == '|')
+		return (c_door);
+	if (pos[1] < height_width[0] - 1 - pos[0] + pad)
 		return (' ');
-	if (j == height_width[0] - 1 - i + pad)
+	if (pos[1] == height_width[0] - 1 - pos[0] + pad)
 		return ('/');
-	if (j == height_width[1] - height_width[0] + i + pad)
+	if (pos[1] == height_width[1] - height_width[0] + pos[0] + pad)
 		return ('\\');
 	return ('*');
 }
 
-void	print_stage(int *height_width, int pad)
+void	print_stage(int stage, int *height_width, int pad)
 {
-	int i;
-	int j;
+	int pos[2];
 
-	i = 0;
-	while (i < height_width[0])
+	pos[0] = 0;
+	while (pos[0] < height_width[0])
 	{
-		j = 0;
-		while (j < height_width[1] - height_width[0] + 1 + i + pad)
+		pos[1] = 0;
+		while (pos[1] < height_width[1] - height_width[0] + 1 + pos[0] + pad)
 		{
-			ft_putchar(what_to_print(i, j, height_width, pad));
-			j++;
+			ft_putchar(what_to_print(stage, pos, height_width, pad));
+			pos[1]++;
 		}
 		ft_putchar('\n');
-		i++;
+		pos[0]++;
 	}
 }
 
@@ -67,7 +81,8 @@ void	sastantua(int size)
 	{
 		dim[0] = 3 + i;
 		dim[1] = calculate_width(i);
-		print_stage(dim, (calculate_width(size - 1) - calculate_width(i)) / 2);
+		print_stage(i, dim,
+					(calculate_width(size - 1) - calculate_width(i)) / 2);
 		i++;
 	}
 }
