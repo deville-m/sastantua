030af6d09f122a73f6fdd2ebdf55311c0dd37948
Matthew DEVILLE <mdeville@e1r11p17.42.fr>
2017-07-09 02:39:40 +0200

    AMAFFFGOOOD IT S WORKING

--- sastantua.c
+++ sastantua.c
@@ -6,11 +6,9 @@
 /*   By: mdeville <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/08 00:54:03 by mdeville          #+#    #+#             */
-/*   Updated: 2017/07/09 02:14:33 by mdeville         ###   ########.fr       */
+/*   Updated: 2017/07/09 02:38:02 by mdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 void	ft_putchar(char c);
 
@@ -18,7 +16,7 @@ int		calculate_width(int stage)
 {
 	if (stage < 0)
 	{
-		return (-1);
+		return (0);
 	}
 	if (stage == 0)
 	{
@@ -29,18 +27,18 @@ int		calculate_width(int stage)
 			2 * (stage + 3));
 }
 
-int		what_to_print(int i, int j, int *height_width)
+int		what_to_print(int i, int j, int *height_width, int pad)
 {
-	if (j < height_width[0] - 1 - i)
+	if (j < height_width[0] - 1 - i + pad)
 		return (' ');
-	if (j == height_width[0] - 1 - i)
+	if (j == height_width[0] - 1 - i + pad)
 		return ('/');
-	if (j == height_width[1] - height_width[0] + i)
+	if (j == height_width[1] - height_width[0] + i + pad)
 		return ('\\');
 	return ('*');
 }
 
-void	print_stage(int *height_width)
+void	print_stage(int *height_width, int pad)
 {
 	int i;
 	int j;
@@ -49,9 +47,9 @@ void	print_stage(int *height_width)
 	while (i < height_width[0])
 	{
 		j = 0;
-		while (j < height_width[1] - height_width[0] + 1 + i)
+		while (j < height_width[1] - height_width[0] + 1 + i + pad)
 		{
-			ft_putchar(what_to_print(i, j, height_width));
+			ft_putchar(what_to_print(i, j, height_width, pad));
 			j++;
 		}
 		ft_putchar('\n');
@@ -69,7 +67,7 @@ void	sastantua(int size)
 	{
 		dim[0] = 3 + i;
 		dim[1] = calculate_width(i);
-		print_stage(dim);
+		print_stage(dim, (calculate_width(size - 1) - calculate_width(i)) / 2);
 		i++;
 	}
 }
