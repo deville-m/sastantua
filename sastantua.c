bf60e48724ea0c363a7e37b196fbc6f5eef5ccb1
Matthew DEVILLE <mdeville@e1r10p17.42.fr>
2017-07-09 15:04:03 +0200

    small fixes to door function

--- sastantua.c
+++ sastantua.c
@@ -6,7 +6,7 @@
 /*   By: mdeville <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/08 00:54:03 by mdeville          #+#    #+#             */
-/*   Updated: 2017/07/09 14:48:05 by mdeville         ###   ########.fr       */
+/*   Updated: 2017/07/09 15:02:12 by mdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,29 +27,6 @@ int		calculate_width(int stage)
 			2 * (stage + 3));
 }
 
-char	print_door(int stage, int i, int j, int pad)
-{
-	int center;
-	int size;
-	int height;
-	int width;
-	
-	height = stage + 3;
-	width = calculate_width(stage);
-	center = width / 2;
-	size = (stage / 2) + 1;
-	if ((stage == 0 && i == 2 + pad && j == 3 + pad) ||
-		(j >= center - size && j <= center + size))
-		return ('|');
-	if (j < height - 1 - i + pad)
-		return (' ');
-	if (j == height - 1 - i + pad)
-		return ('/');
-	if (j == width - height + i + pad)
-		return ('\\');
-	return ('*');
-}
-
 char	what_to_print(int i, int j, int *height_width, int pad)
 {
 	if (j < height_width[0] - 1 - i + pad)
@@ -59,6 +36,19 @@ char	what_to_print(int i, int j, int *height_width, int pad)
 	if (j == height_width[1] - height_width[0] + i + pad)
 		return ('\\');
 	return ('*');
+}
+
+char	door(int stage, int i, int j, int *height_width, int pad)
+{
+	int center;
+	int size;
+
+	center = height_width[1] / 2;
+	size = (stage / 2);
+	if ((stage == 0 && i == 2 + pad && j == 3 + pad) ||
+		(j >= center - size && j <= center + size))
+		return ('|');
+	return what_to_print(i, j, height_width, pad);
 }
 
 void	print_stage(int stage, int size, int *height_width, int pad)
@@ -72,7 +62,7 @@ void	print_stage(int stage, int size, int *height_width, int pad)
 		while (pos[1] < height_width[1] - height_width[0] + 1 + pos[0] + pad)
 		{
 			if (stage == size)
-				ft_putchar(print_door(stage, pos[0], pos[1], pad));
+				ft_putchar(door(stage, pos[0], pos[1], height_width, pad));
 			else
 				ft_putchar(what_to_print(pos[0], pos[1], height_width, pad));
 			pos[1]++;
